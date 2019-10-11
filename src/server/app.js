6de10d28377b844dabc175a6c37e12dfb77750dd
js87zz <js87zz.lee@nhn.com>
2019-10-11 18:45:04 +0900

    docs: change grid example code to commucate with v4.6.1

--- src/server/app.js
+++ src/server/app.js
@@ -7,15 +7,15 @@ const port = 3000;
 const contents = [
   {
     id: 1,
-    date: '2018-11-19',
+    date: '2019-11-27',
     category1: '1',
     category2: '웨일즈마켓',
-    payment: '4',
+    payment: '2',
     amount: '5000'
   },
   {
     id: 2,
-    date: '2018-11-19',
+    date: '2019-11-27',
     category1: '3',
     category2: '택시',
     payment: '1',
@@ -23,7 +23,7 @@ const contents = [
   },
   {
     id: 3,
-    date: '2018-11-19',
+    date: '2019-11-27',
     category1: '2',
     category2: '영화',
     payment: '3',
@@ -32,24 +32,27 @@ const contents = [
 ];
 
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.post('/api/readData', (req, res) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-
-  return res.send({
-    result: true,
-    data: {
-      contents
-    }
-  });
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  // allow preflight
+  if (req.method === 'OPTIONS') {
+    res.send(200);
+  } else {
+    next();
+  }
 });
 
-app.post('/api/updateData', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+app.get('/api/readData', (req, res) => res.send({
+  result: true,
+  data: {
+    contents
+  }
+}));
 
-  const rows = JSON.parse(req.body.updatedRows);
+app.put('/api/updateData', (req, res) => {
+  const rows = req.body.updatedRows;
 
   rows.forEach(row => {
     const index = row.id - 1;
